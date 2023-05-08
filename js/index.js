d075c541d977c04d32b4b4cd64f3a24e5e2a7bc8
Pierre-Marie Marchio <pierremariemarchio@gmail.com>
2023-05-08 15:35:49 +0200

    target current button

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
+
+
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", () => {
-    var audio = new Audio("./assets/sounds/tom-1.mp3");
-    audio.play();
+  document.querySelectorAll(".drum")[i].addEventListener("click", (e) => {
+    e.currentTarget.style.color = "white";
   });
 }
+// var audio = new Audio("./assets/sounds/tom-1.mp3");
+//     audio.play();
