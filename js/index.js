0081c6dfbd6ca9b46451590f9714d9a3b6aac12d
Pierre-Marie Marchio <pierremariemarchio@gmail.com>
2023-05-08 17:09:17 +0200

    change switch whith object

--- js/index.js
+++ js/index.js
@@ -1,39 +1,26 @@
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", (e) => {
-    var btnInnerHTML = e.currentTarget.innerHTML;
+const audioMap = {
+  z: "./assets/sounds/tom-1.mp3",
+  q: "./assets/sounds/tom-2.mp3",
+  s: "./assets/sounds/tom-3.mp3",
+  d: "./assets/sounds/tom-4.mp3",
+  j: "./assets/sounds/snare.mp3",
+  k: "./assets/sounds/crash.mp3",
+  l: "./assets/sounds/kick-bass.mp3",
+};
 
-    switch (btnInnerHTML) {
-      case "w":
-        var audio = new Audio("./assets/sounds/tom-1.mp3");
-        audio.play();
-        break;
-      case "a":
-        var audio = new Audio("./assets/sounds/tom-2.mp3");
-        audio.play();
-        break;
-      case "s":
-        var audio = new Audio("./assets/sounds/tom-3.mp3");
-        audio.play();
-        break;
-      case "d":
-        var audio = new Audio("./assets/sounds/tom-4.mp3");
-        audio.play();
-        break;
-      case "j":
-        var audio = new Audio("./assets/sounds/snare.mp3");
-        audio.play();
-        break;
-      case "k":
-        var audio = new Audio("./assets/sounds/crash.mp3");
-        audio.play();
-        break;
-      case "l":
-        var audio = new Audio("./assets/sounds/kick-bass.mp3");
-        audio.play();
-        break;
-      default:
-        break;
-        console.log("Invalid input type");
-    }
+function switchDrum(key) {
+  const audio = new Audio(audioMap[key]);
+  audio.play();
+}
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", (e) => {
+    const btnInnerHTML = e.currentTarget.innerHTML;
+    switchDrum(btnInnerHTML);
   });
 }
+
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+  switchDrum(key);
+});
